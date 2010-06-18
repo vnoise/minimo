716ff7db41fe2244de9a079a0ff330e88aa69a85
Matthias Georgi <matti.georgi@gmail.com>
2010-06-18 22:23:20 +0200

    effects

--- machine1.js
+++
@@ -1,8 +0,0 @@
-[
-{address: '/volume', args: [0,1]},
-{address: '/sinus', args: [0, 0.5]},
-{address: '/freq', args: [0, 100]},
-{address: '/noise', args: [0, 0.5]},
-{address: '/attack', args: [0, 0]},
-{address: '/decay', args: [0, 100]}
-]
